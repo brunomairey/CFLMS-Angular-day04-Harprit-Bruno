56a023a90c476064aa3c3bf52b949deacce6f6bf
brunomairey <bruno.mairey@yahoo.fr>
2020-06-25 11:18:08 +0200

    creation of first pages

---
+++ blog/src/app/blogentries.ts
@@ -0,0 +1,27 @@
+export const blogentries = [
+	{
+		place:"Grossvenediger",
+		date: "16.04.2018",
+		time: "09:00",
+		author:"Hurprit",
+		created: "25.06.2020",
+		description:"A classical mountain that will push your limits",
+		img:"../assets/Venediger.jpg",
+},{
+		place:"Osttyrol",
+		date: "16.04.2019",
+		time: "10:00",
+		author:"Bruno",
+		created: "24.06.2020",
+		description:"Enjoy the beginning of spring in the mountain",
+		img:"../assets/Osttyrol.jpg",
+	},{
+		place:"Hospiz",
+		date: "03.04.2019",
+		time: "08:00",
+		author:"Hurprit",
+		created: "23.06.2019",
+		description:"Enjoy a mountainering experience",
+		img:"../assets/Hospiz.jpg",
+	},
+];
---
+++ blog/src/app/home/home.component.ts
@@ -0,0 +1,16 @@
+import { Component, OnInit } from '@angular/core';
+import { blogentries } from '../blogentries';
+
+@Component({
+  selector: 'home',
+  templateUrl: './home.component.html',
+  styleUrls: ['./home.component.sass']
+})
+export class HomeComponent implements OnInit {
+	articles = blogentries;
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
