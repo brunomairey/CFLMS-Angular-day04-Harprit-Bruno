56cbb879048ac965fb54eda8aa1e954ed932d7d6
HarpritSmn <64331910+HarpritSmn@users.noreply.github.com>
2020-06-25 13:18:34 +0200

    added formular to contact us

--- blog/src/app/app.module.ts
+++ blog/src/app/app.module.ts
@@ -7,6 +7,7 @@ import { NavbarComponent } from './navbar/navbar.component';
 import { HomeComponent } from './home/home.component';
 import { AboutComponent } from './about/about.component';
 import { ContactComponent } from './contact/contact.component';
+import { ReactiveFormsModule } from '@angular/forms';
 
 @NgModule({
   declarations: [
@@ -18,7 +19,8 @@ import { ContactComponent } from './contact/contact.component';
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    ReactiveFormsModule
   ],
   providers: [],
   bootstrap: [AppComponent]
--- blog/src/app/contact/contact.component.ts
+++ blog/src/app/contact/contact.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { FormBuilder } from '@angular/forms';
 
 @Component({
   selector: 'contact',
@@ -6,10 +7,24 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./contact.component.sass']
 })
 export class ContactComponent implements OnInit {
+  checkoutForm;
 
-  constructor() { }
+  constructor(private formBuilder: FormBuilder) {
+    this.checkoutForm = this.formBuilder.group({
+      FirstName: '',
+      LastName: '',
+      address: ''
+    });
+   }
 
-  ngOnInit(): void {
+  ngOnInit() {
+  }
+  onSubmit(customerData) {
+    // Process checkout data here
+    alert('We received your Data');
+ 
+    // this.items = this.cartService.clearCart();
+    this.checkoutForm.reset();
   }
 
 }
